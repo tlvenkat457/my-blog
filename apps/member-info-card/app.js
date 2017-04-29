5a8a3dd8a3f3c605aca9d2214577d942d7b02c70
venkat <lakshmivenkatt@gmail.com>
2017-04-29 19:15:36 -0400

    Deleted resume from the home page

--- apps/member-info-card/app.js
+++ apps/member-info-card/app.js
@@ -31,7 +31,22 @@ angular.module('app').controller('mainCtrl',function($scope){
             'Nithai'
         ]
     }
+
+    $scope.user3 = {name:"Nedd Stark", address : {
+        street : "65 North point drive",
+        city : "Boston",
+        state : "MA",
+        country: "USA"
+    },
+        friends: [
+            'John Snow',
+            'Angela',
+            'Avon'
+        ]
+    }
 });
+
+
 
 angular.module('app').directive('userInfoCard',function(){
 
@@ -39,16 +54,29 @@ angular.module('app').directive('userInfoCard',function(){
         templateUrl: "userInfoCard.prtl.html",
         restrict : "E",
         scope: {
-            user : "="
+            user : "=",
+            initialCollapsed : "@collapsed"
         },
         controller : function($scope){
 
-            $scope.collapsed = false;
+            $scope.collapsed = ($scope.initialCollapsed == "true");
+
             $scope.knightMe = function(user){
                 user.rank = "knight";
             };
+
             $scope.collapse = function(){
                 $scope.collapsed = !$scope.collapsed;
+            }
+
+            $scope.$on('deleteUser', function (event, data) {
+                console.log(event);
+                console.log(data);
+                window.alert("user deleted "+data);
+            });
+
+            $scope.deleteUser = function(userToDel){
+                $scope.$emit('deleteUser', userToDel);
             }
         }
     }
@@ -60,13 +88,16 @@ angular.module('app').directive('address', function(){
     return {
         templateUrl: "address.prtl.html",
         restrict : "E",
+        scope : true,
         controller: function($scope){
-            $scope.addressCollapsed = false;
+            $scope.collapsed = true;
+            //$scope.
             $scope.collapseAddress = function () {
-                $scope.addressCollapsed = !$scope.addressCollapsed;
+                $scope.collapsed = true;
             }
             $scope.expandAddress = function () {
-                $scope.addressCollapsed = !$scope.addressCollapsed;
+                $scope.$emit('deleteUser', {name:'rama'});
+                $scope.collapsed = false;
             }
         }
     }
